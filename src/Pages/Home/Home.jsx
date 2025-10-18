9fd05fe4c0792965059fb518a4d74c2fe2918822
Gyandeep <gyandeep@flyde.in>
2025-10-18 13:34:57 +0530

    Login Page done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import styles from "./Home.module.css";
 import RegisterForm from "../../component/RegisterForm/RegisterForm";
 import LoginForm from "../../component/LoginForm/LoginForm";
 import { useSelector } from "react-redux";
+import map from "../../assets/mapMetro.png"
 // import { AnimatePresence, motion } from "framer-motion";
 
 import { Fingerprint, Home, Lock, Mail, Phone, UserRound,TramFront } from "lucide-react";
@@ -14,9 +15,9 @@ import { Fingerprint, Home, Lock, Mail, Phone, UserRound,TramFront } from "lucid
 function home() {
   const currentForm = useSelector((state) => state.authUI.currentForm);
 
-
-
   return (
+
+    
     <div className={styles.page}>
       <div className={styles.layout}>
 
@@ -29,6 +30,9 @@ function home() {
             </span>
             <span className={styles.logoText}>MetroApp</span>
           </header>
+          <div className={styles.map}>
+            <img src={map} alt="" />
+          </div>
           <div className={styles.heroBody}>
             <h1 className={styles.heroTitle}>Your city, your metro.</h1>
             <p className={styles.heroSubtitle}>
